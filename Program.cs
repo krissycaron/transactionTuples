f06247676f165d69619029239c5af8d255aa9ae4
Kristin Caron <krissycaron@gmail.com>
2017-07-11 11:52:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace transactionTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+                transactions.Add("bowl",3.50,6);
+                transactions.Add("spoon",1.20,3);
+                transactions.Add("fork",1.20,4);
+                transactions.Add("knife",2.30,5);
+                transactions.Add("plate",4.20,7);
+            foreach(transaction in transactions){
+                Console.Write(transaction);
+            }
+
+        }
+    }
+}
