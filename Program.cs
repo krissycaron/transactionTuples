7741c529b0b0ae0ab2f01a633cb5d3db20fa7a3a
Kristin Caron <krissycaron@gmail.com>
2017-07-11 12:55:42 -0500

    writeing total amount sold patrially to console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace transactionTuples
 {
@@ -7,14 +8,19 @@ namespace transactionTuples
         static void Main(string[] args)
         {
             List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
-                transactions.Add("bowl",3.50,6);
-                transactions.Add("spoon",1.20,3);
-                transactions.Add("fork",1.20,4);
-                transactions.Add("knife",2.30,5);
-                transactions.Add("plate",4.20,7);
-            foreach(transaction in transactions){
-                Console.Write(transaction);
+                transactions.Add(("bowl", 3.50, 6));
+                transactions.Add(("spoon", 1.20, 3));
+                transactions.Add(("fork", 1.20, 4));
+                transactions.Add(("knife", 2.30, 5));
+                transactions.Add(("plate", 4.20, 7));
+            foreach((string product, double amount, int quantity)transaction in transactions){
+                Console.WriteLine($"{transaction.amount} {transaction.quantity}");
+               double totalAmount = transaction.amount * transaction.quantity;
+               Console.WriteLine($"totalAmount");
             }
+                
+            // Console.WriteLine($"{product.Item1} {product.Item2} {product.Item3} ");
+
 
         }
     }
